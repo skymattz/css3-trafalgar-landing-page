2dc01addeceedd2ac304a30cc84de27a171d9eb5
Amat <amatid404@gmail.com>
2021-01-27 12:10:04 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+const toggleIcon = document.querySelector('.nav-toggle i');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    toggleIcon.classList.toggle('fa-times');
+});
